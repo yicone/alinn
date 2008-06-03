64f4e20b3e85bc146e646381b406946e927d15aa
dcyuan480@163.com <dcyuan480@163.com@03639c57-424a-0410-b296-61f97550bc55>
2008-06-03 06:51:39 +0000

    添加网站主部分交易管理页面；

---
+++ Web/Member/Zone/OrderViewer.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace Web.Member.Zone
+{
+    public partial class OrderViewer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            if (!Page.IsPostBack)
+            {
+                //First initialize the page
+                //DataList1.DataBind();
+            }
+            else
+            {
+                //When post back\
+                DataList1.DataBind();
+            }
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            //Creat a query about
+        }
+
+        protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+            e.Command.Parameters[0].Value = HOT.BLL.User.GetLoginUser(); //UNDONE: JUST FOR TEST
+        }
+
+        protected void SqlDataSource2_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+            e.Command.Parameters[0].Value = HOT.BLL.User.GetLoginUser(); //UNDONE: JUST FOR TEST
+            if (!Page.IsPostBack)
+            {
+                e.Command.Parameters[1].Value = Guid.Empty;
+                e.Command.Parameters[2].Value = " ";
+                e.Command.Parameters[3].Value = " ";
+            }
+            else
+            {
+                e.Command.Parameters[1].Value = this.DropDownList1.SelectedValue;
+                e.Command.Parameters[2].Value = this.TextBox1.Text;
+                e.Command.Parameters[3].Value = this.TextBox2.Text;
+            }
+        }
+    }
+}
