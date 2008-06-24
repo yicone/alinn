0d83ec13d2a9b21017a7afc68036fb16062c2943
yicone@gmail.com <yicone@gmail.com@03639c57-424a-0410-b296-61f97550bc55>
2008-06-24 20:41:45 +0000

    修正多个广告位显示问题； 修正图片广告位链接无法点击问题； 修正广告位显示位置问题；

--- Web/JavaScript/inf.js
+++ Web/JavaScript/inf.js
@@ -1,35 +1,76 @@
 ﻿function generateIFrame(){
-    var query = "titlecolor=" + aaaa0_titlecolor;
-    query = query + "&textcolor=" + aaaa0_textcolor;
-    query = query + "&linkcolor=" + aaaa0_linkcolor;
-    query = query + "&bordercolor=" + aaaa0_bordercolor;
-    query = query + "&backgroundcolor=" + aaaa0_backgroundcolor;
-    query = query + "&groundingcolor=" + aaaa0_groundingcolor;
-    query = query + "&cornerstyle=" + aaaa0_cornerstyle;
-    query = query + "&backgroundimage=" + aaaa0_backgroundimage;
-    query = query + "&iconimage=" + aaaa0_iconimage;
-    query = query + "&sizecode=" + aaaa0_sizecode;
-    query = query + "&layouttype=" + aaaa0_layouttype;
+    var query = new StringBuilder("titlecolor=");
+    query.append(aaaa0_titlecolor);
+    query.append("&textcolor=");
+    query.append(aaaa0_textcolor);
+    query.append("&linkcolor=");
+    query.append(aaaa0_linkcolor);
+    query.append("&bordercolor=");
+    query.append(aaaa0_bordercolor);
+    query.append("&backgroundcolor=");
+    query.append(aaaa0_backgroundcolor);
+    query.append("&groundingcolor=");
+    query.append(aaaa0_groundingcolor);
+    query.append("&cornerstyle=");
+    query.append(aaaa0_cornerstyle);
+    query.append("&backgroundimage=");
+    query.append(aaaa0_backgroundimage);
+    query.append("&iconimage=");
+    query.append(aaaa0_iconimage);
+    query.append("&sizecode=");
+    query.append(aaaa0_sizecode);
+    query.append("&layouttype=");
+    query.append(aaaa0_layouttype);
     
-    query = query + "&zoneid=" + aaaa0_pid;
-    query = query + "&zonehref=" + location.href;
+    query.append("&zoneid=")
+    query.append(aaaa0_pid);
+    query.append("&zonehref=")
+    query.append(location.href);
 
-	var dframe = document.createElement("IFRAME");
-	var jframe = $(dframe);
-	jframe.attr("name", "a");
-	jframe.attr("id", "");
-	jframe.attr("scrolling", "no");
-	jframe.attr("frameborder", "0");
-	var size = aaaa0_sizecode.split("x");
-	var width = size[0];
-	var height = size[1];
-	jframe.attr("width", width);
-	jframe.attr("height", height);
-	jframe.attr("marginheight", "0");
-	jframe.attr("marginwidth", "0");
-	jframe.attr("border", "0");
-	jframe.attr("src", "http://www.aaaa0.com/Public/ZoneOutput.aspx?" + query);
-	$("body").append(jframe);
+//	var dframe = document.createElement("IFRAME");
+//	var jframe = $(dframe);
+//	jframe.attr("name", "a");
+//	jframe.attr("id", "");
+//	jframe.attr("scrolling", "no");
+//	jframe.attr("frameborder", "0");
+//	var size = aaaa0_sizecode.split("x");
+//	var width = size[0];
+//	var height = size[1];
+//	jframe.attr("width", width);
+//	jframe.attr("height", height);
+//	jframe.attr("marginheight", "0");
+//	jframe.attr("marginwidth", "0");
+//	jframe.attr("border", "0");
+//	jframe.attr("src", "http://www.aaaa0.com/Public/ZoneOutput.aspx?" + query);
+//	alert(dframe.innerHtml);
+//	$("body").append(jframe);
+
+    var size = aaaa0_sizecode.split("x");
+    var width = size[0];
+    var height = size[1];
+    var sb = new StringBuilder('<iframe name="a" id="" scrolling="no" frameborder="0" marginheight="0" marginwidth="0" border="0" ');
+    sb.append(' width="' + width + '"' );
+    sb.append(' height="' + height + '"' );
+    sb.append(' src="http://www.aaaa0.com/Public/ZoneOutput.aspx?' + query + '" ></iframe>');
+    document.write(sb.toString());
+} 
+
+//提高字符串合并的性能
+function StringBuilder(value){
+    this.strings = new Array("");
+    this.append(value);
 }
 
-$(document).ready(generateIFrame);
+StringBuilder.prototype.append = function(s){
+    if (s) {
+        this.strings.push(s);
+    }
+}
+StringBuilder.prototype.clear = function(){
+    this.strings.length = 1;
+}
+StringBuilder.prototype.toString = function(){
+    return this.strings.join("");
+}
+
+generateIFrame();
